56310f47ed8f83b23c4f7c92a9f1d39f5ea248f4
Idriss Animashaun <idriss.animashaun@intel.com>
2022-06-07 12:12:22 +0100

    update rev and remove unused files

--- main/Inputs/Test_Program.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-class Test_Program(object):
-    def __init__(self, name_input, dir_input, engID_input, operation_input):
-        self.name = name_input;
-        self.dir = dir_input;
-        self.engID = engID_input;
-        self.op = operation_input;
